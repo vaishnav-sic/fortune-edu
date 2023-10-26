17f763286c8c7a5b3044484d7820dcf616c0d741
Prathamesh Godse <prathameshgodse00@gmail.com>
2023-10-26 23:24:18 +0530

    contact page with added logo in main branch and mock test page

---
+++ components/MockDetails.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import IMG from "../public/assets/images/Fortune-Edu-logo.png"
+
+const MockDetails = () => {
+    return (        
+     <div>
+                     
+        <div className="cta-two">
+            <div className="container-fluid">
+                <div className="row no-gutters">
+                    <div className="col-lg-6 thm-base-bg">
+                        <div className="cta-two__single_mock">
+                            <div className="cta-two__icon">
+                                <span><i className="kipso-icon-professor"></i></span>
+                            </div>
+                            <div className="cta-two__content">
+                                <h2 className="cta-two__title">MEDICAL</h2>
+                                <p className="cta-two__text">There are many variations of passages of lore available
+                                    but <br /> the
+                                        majority have suffered alteration in some form.</p>
+                                <a href="#" className="thm-btn cta-two__btn">Run Mock Test</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-lg-6 thm-base-bg-2">
+                        <div className="cta-two__single_mock">
+                            <div className="cta-two__icon">
+                                <span><i className="kipso-icon-knowledge"></i></span>
+                            </div>
+                            <div className="cta-two__content">
+                                <h2 className="cta-two__title">ENGINEERING</h2>
+                                <p className="cta-two__text">There are many variations of passages of lore available
+                                    but <br /> the
+                                        majority have suffered alteration in some form.</p>
+                                <a href="#" className="thm-btn cta-two__btn">Run Mock Test</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        </div>
+    );
+};
+
+
+
+export default MockDetails;
