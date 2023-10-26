f046ebc49f8832360c8930b66c538e99ba71a53b
Rushikesh Pote <rushi5059@gmail.com>
2023-10-26 15:52:56 +0530

    Hover animation change for Registration button

--- components/SliderOne.js
+++ components/SliderOne.js
@@ -1,92 +1,127 @@
-import React, {useState} from 'react';
-import Swiper from 'react-id-swiper';
-import 'swiper/css/swiper.css';
+import React, { useState } from "react";
+import Swiper from "react-id-swiper";
+import "swiper/css/swiper.css";
 import Slider from "react-slick";
-import 'slick-carousel/slick/slick.css';
-import 'slick-carousel/slick/slick-theme.css'
-import NumberAni from './NumberAni';
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import NumberAni from "./NumberAni";
 
 const SliderOne = () => {
+  const [swiper, setSwiper] = useState(null);
 
-    const [swiper, setSwiper] = useState(null);
+  const goNext = () => {
+    if (swiper !== null) {
+      swiper.slideNext();
+    }
+  };
 
-    const goNext = () => {
-        if (swiper !== null) {
-          swiper.slideNext();
-        }
-    };
+  const goPrev = () => {
+    if (swiper !== null) {
+      swiper.slidePrev();
+    }
+  };
 
-    const goPrev = () => {
-        if (swiper !== null) {
-          swiper.slidePrev();
-        }
-    };
-
-
-    return (
-        <div className="banner-wrapper">
-            <section className="banner-one banner-carousel__one no-dots">
-                <Swiper getSwiper={setSwiper}>
-                    <div className="banner-one__slide banner-one__slide-one">
-                        <div className="container">
-                            <div className="banner-one__bubble-1"></div>
-                            <div className="banner-one__bubble-2"></div>
-                            <div className="banner-one__bubble-3"></div>
-                            <img src="/assets/images/doctor.png" alt="" className="banner-one__scratch" /> 
-                           {/* <img src="/assets/images/Engineer.jpg" className="banner-one__person" alt="" /> */}
-                            <div className="row no-gutters">
-                                <div className="col-xl-12">
-                                    <h3 className="banner-one__title banner-one__light-color">For guaranteed<br />
-                                    admissions</h3>
-                                    <p className="banner-one__tag-line">to medical & engineering </p>
-                                        <p className="banner-one__tag-line-one">Experts in hassle-free admissions
-                                           <br/>Recommended by thousands of Doctors and Engineers</p>
-                                   <button className='btn'><a href="#registration1" className="thm-btn banner-one__btn">Registration</a></button> 
-                                </div>                                
-                            </div> 
-                        </div>
-                    </div>
-                    <div className="banner-one__slide banner-one__slide-two">
-                        <div className="container">
-                            <div className="banner-one__bubble-1"></div>
-                            <div className="banner-one__bubble-2"></div>
-                            <div className="banner-one__bubble-3"></div>
-                            <img src="/assets/images/engineering-students.jpg" className="banner-one__person" alt="" />
-                            {/* <img src="/assets/images/slider-1-person-2.png" className="banner-one__person" alt="" /> */} 
-                            <div className="row no-gutters">
-                                <div className="col-xl-12">
-                                    <h3 className="banner-one__title banner-one__light-color">We Can <br />
-                                        Teach You</h3>
-                                    <p className="banner-one__tag-line">Education is the best key to success</p>
-                                    {/* <a href="#" className="thm-btn banner-one__btn">Learn More</a> */}
-                                </div>
-                                {/* <div className='about-two-main_count'>
+  return (
+    <div className="banner-wrapper">
+      <section className="banner-one banner-carousel__one no-dots">
+        <Swiper getSwiper={setSwiper}>
+          <div className="banner-one__slide banner-one__slide-one">
+            <div className="container">
+              <div className="banner-one__bubble-1"></div>
+              <div className="banner-one__bubble-2"></div>
+              <div className="banner-one__bubble-3"></div>
+              <img
+                src="/assets/images/doctor.png"
+                alt=""
+                className="banner-one__scratch"
+              />
+              {/* <img src="/assets/images/Engineer.jpg" className="banner-one__person" alt="" /> */}
+              <div className="row no-gutters">
+                <div className="col-xl-12">
+                  <h3 className="banner-one__title banner-one__light-color">
+                    For guaranteed
+                    <br />
+                    admissions
+                  </h3>
+                  <p className="banner-one__tag-line">
+                    to medical & engineering{" "}
+                  </p>
+                  <p className="banner-one__tag-line-one">
+                    Experts in hassle-free admissions
+                    <br />
+                    Recommended by thousands of Doctors and Engineers
+                  </p>
+                  <button className="btn">
+                    <a
+                      href="#registration1"
+                      className="thm-btn banner-one__btn"
+                    >
+                      Book Consultation
+                    </a>
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="banner-one__slide banner-one__slide-two">
+            <div className="container">
+              <div className="banner-one__bubble-1"></div>
+              <div className="banner-one__bubble-2"></div>
+              <div className="banner-one__bubble-3"></div>
+              <img
+                src="/assets/images/engineering-students.jpg"
+                className="banner-one__person"
+                alt=""
+              />
+              {/* <img src="/assets/images/slider-1-person-2.png" className="banner-one__person" alt="" /> */}
+              <div className="row no-gutters">
+                <div className="col-xl-12">
+                  <h3 className="banner-one__title banner-one__light-color">
+                    We Can <br />
+                    Teach You
+                  </h3>
+                  <p className="banner-one__tag-line">
+                    Education is the best key to success
+                  </p>
+                  {/* <a href="#" className="thm-btn banner-one__btn">Learn More</a> */}
+                </div>
+                {/* <div className='about-two-main_count'>
                                 <NumberAni num="16784" name = "Students Admitted"/>
                                 <NumberAni num="25532" name = "Students Counselled"/> 
                                 <NumberAni num="20" name = "Years Of Experience"/>
                                 </div> */}
-                            </div>
-                        </div>
-                    </div>
-                </Swiper>
-            </section>
-            <div className="banner-carousel-btn">
-                <div onClick={goPrev} className="banner-carousel-btn__left-btn banner-arrow"><i className="kipso-icon-left-arrow"></i></div>
-                <div onClick={goNext} className="banner-carousel-btn__right-btn banner-arrow"><i className="kipso-icon-right-arrow"></i></div>
+              </div>
             </div>
-            {/* <div className="banner-one__cta">
+          </div>
+        </Swiper>
+      </section>
+      <div className="banner-carousel-btn">
+        <div
+          onClick={goPrev}
+          className="banner-carousel-btn__left-btn banner-arrow"
+        >
+          <i className="kipso-icon-left-arrow"></i>
+        </div>
+        <div
+          onClick={goNext}
+          className="banner-carousel-btn__right-btn banner-arrow"
+        >
+          <i className="kipso-icon-right-arrow"></i>
+        </div>
+      </div>
+      {/* <div className="banner-one__cta">
                 <div className="banner-one__cta-icon">
                     <i className="kipso-icon-black-graduation-cap-tool-of-university-student-for-head"></i>
                 </div> */}
-                {/* <div className="banner-one__cta-title">
+      {/* <div className="banner-one__cta-title">
                     <h3 className="banner-one__cta-text"><a href="#">Read how we encourage our
                         students to learn</a></h3>
                 </div> */}
-                {/* <div className="banner-one__cta-link">
+      {/* <div className="banner-one__cta-link">
                     <a href="#"><i className="kipso-icon-right-arrow"></i></a>
                 </div> */}
-            {/* </div> */}
-        </div>
-    );
-}
+      {/* </div> */}
+    </div>
+  );
+};
 export default SliderOne;
--- pages/index.js
+++ pages/index.js
@@ -16,40 +16,36 @@ import SubscribeOne from "../components/SubscribeOne";
 import NumberAni from "../components/NumberAni";
 import AboutUs from "../components/AboutUs";
 
-import NeetScoreAnalysis from "../components/NeetScorAnalysis"
-import Nridoc from "../components/Nridoc"
-import OneToOneCons from "../components/OneToOneCons"
-import Registration from "../components/Registration"
-import Documentation from "../components/Documentation"
-import AdmissionAlert from "../components/AdmissionAlert"
-import AdmissionFillingForm from "../components/AdmissionFillingForm"
-import Admissionguidance from "../components/Admissionguidance"
-import CollegeChoicePref from "../components/CollegeChoicePref"
-
+import NeetScoreAnalysis from "../components/NeetScorAnalysis";
+import Nridoc from "../components/Nridoc";
+import OneToOneCons from "../components/OneToOneCons";
+import Registration from "../components/Registration";
+import Documentation from "../components/Documentation";
+import AdmissionAlert from "../components/AdmissionAlert";
+import AdmissionFillingForm from "../components/AdmissionFillingForm";
+import Admissionguidance from "../components/Admissionguidance";
+import CollegeChoicePref from "../components/CollegeChoicePref";
 
 const HomePage = () => (
-
-    <Layout pageTitle="Fortune Education Services">
-      {/* <Topbar /> */}
-      <NavOne />
-      <SliderOne />
-      <AboutTwo />
-      <CourseOne />
-      {/* <VideoTwo /> */}
-      <CountdownKipso />
-      {/* <CourseCatOne /> */}
-      {/* <CallToActionThree /> */}
-      {/* <AboutUs /> */}
-      {/* <BrandsTwo /> */}
-      {/* <BlogTwo /> */}
-      {/* <CallToActionFour />
+  <Layout pageTitle="Fortune Education Services">
+    {/* <Topbar /> */}
+    <NavOne />
+    <SliderOne />
+    <AboutTwo />
+    <CourseOne />
+    {/* <VideoTwo /> */}
+    <CountdownKipso />
+    {/* <CourseCatOne /> */}
+    {/* <CallToActionThree /> */}
+    {/* <AboutUs /> */}
+    {/* <BrandsTwo /> */}
+    {/* <BlogTwo /> */}
+    {/* <CallToActionFour />
       <SubscribeOne /> */}
-      <Footer />
-      {/* <NumberAni num="16784" name = "Students Admitted"/> */}
-       
-    </Layout>
-
-)
+    <Footer />
+    {/* <NumberAni num="16784" name = "Students Admitted"/> */}
+  </Layout>
+);
 
 export default HomePage;
 /* start  */
