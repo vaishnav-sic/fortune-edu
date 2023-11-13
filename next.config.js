27e5ef9d249fa9fd26b8b622c4e4ec75a8f31051
vaishnav-sic <vaishnavwirkar2011@gmail.com>
2023-11-13 11:54:21 +0530

    Export out

--- next.config.js
+++ next.config.js
@@ -1,17 +1,24 @@
 const withSass = require('@zeit/next-sass')
 const withCSS = require("@zeit/next-css");
-module.exports = withCSS(withSass({
+const nextConfig = {
+    output: 'export',
+    // more configuration options can be added here
+  };
+  
+  module.exports = withCSS(withSass({
     webpack(config, options) {
-        config.module.rules.push({
-            test: /\.(png|jpg|gif|svg|eot|ttf|woff|woff2)$/,
-            use: {
-                loader: 'url-loader',
-                options: {
-                    limit: 100000
-                }
-            }
-        });
-
-        return config;
-    }
-}));
+      config.module.rules.push({
+        test: /\.(png|jpg|gif|svg|eot|ttf|woff|woff2)$/,
+        use: {
+          loader: 'url-loader',
+          options: {
+            limit: 100000,
+          },
+        },
+      });
+  
+      return config;
+    },
+    // merge the existing configuration with nextConfig
+    ...nextConfig,
+  }));
