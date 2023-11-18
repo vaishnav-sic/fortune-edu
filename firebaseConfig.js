dc19c513574b5213b7e3a000e1a476414b06956c
rupesh141997 <rupeshborkar14@gmail.com>
2023-11-19 01:42:53 +0530

    Admin Login component added

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -3,6 +3,7 @@ import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getFirestore } from "firebase/firestore"
 import { getStorage } from "firebase/storage";
+import { getAuth } from 'firebase/auth';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -21,5 +22,7 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 const storage = getStorage(app);
+const auth = getAuth(app);
  
 export { db, storage };
+export default auth;
