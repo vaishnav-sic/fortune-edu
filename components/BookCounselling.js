1cda1121c954fddf781609d5ea11a6d3b5b2afa9
hemantmj <hmjaiswal0@gmail.com>
2023-12-22 17:18:40 +0530

    added official account to send otp

--- components/BookCounselling.js
+++ components/BookCounselling.js
@@ -137,13 +137,15 @@ const BookCounselling = () => {
       myHeaders.append("Content-Type", "application/json");
       myHeaders.append(
         "Authorization",
-        "Basic ajNxdU90M3dhdWU2QWJTOFA0aUE6alRRRlVzN0ZMNVBrRTc3ZW51ZXA4aVNKQmRSaDQ4clp1WVB6eWYwMg=="
+        "Basic OHJPdWpzaHgybEhPUldKeXl2WFU6bTc1U29DVXFsU2tOWndvaFhSMmZWWnFSdW41NXJZSlBCRFZscFVYMA=="
       );
 
       var raw = JSON.stringify({
-        Text: "User Admin login OTP is " + randomOtp + " - SMSCNT",
+        Text:
+          randomOtp +
+          " is the OTP to verify your mobile number at FORTUNE EDUCATION. It is valid for 10 mins. OTPs are CONFIDENTIAL. DO NOT disclose it to anyone.",
         Number: "91" + formData.mobile,
-        SenderId: "SMSCNT",
+        SenderId: "FRTEDU",
         DRNotifyUrl: "https://www.domainname.com/notifyurl",
         DRNotifyHttpMethod: "POST",
         Tool: "API",
@@ -157,14 +159,10 @@ const BookCounselling = () => {
       };
 
       fetch(
-        "https://restapi.smscountry.com/v0.1/Accounts/j3quOt3waue6AbS8P4iA/SMSes/",
+        "https://restapi.smscountry.com/v0.1/Accounts/8rOujshx2lHORWJyyvXU/SMSes/",
         requestOptions
       )
-        .then((response) => {
-          response.text();
-          setEnterOtp(true);
-          setGetOtp(false);
-        })
+        .then((response) => response.text())
         .then((result) => console.log(result))
         .catch((error) => console.log("error", error));
     }
